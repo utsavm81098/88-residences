42ffcb064284f6f93c318dc347419f5cbeabee19
Utsav Modi <utsavm@itpathsolutions.com>
2026-06-04 10:21:01 +0530

    fix the phone regex issues

--- src/utils/validation/regex.js
+++ src/utils/validation/regex.js
@@ -3,7 +3,7 @@
  */
 
 // Phone number regex: supports +, spaces, and digits (min 6 digits)
-export const PHONE_REGEX = /^[+0-9\s]{6,}$/;
+export const PHONE_REGEX = /^(?=(?:.*\d){6,})\+?[0-9][0-9\s\-()]{5,19}$/;
 
 // Email regex is handled by Zod's .email(), but if we need a custom one:
 export const EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
